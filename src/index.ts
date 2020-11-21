cfb94573b8ce03fada8a16a517859af41a4a666a
Marcelo Del Negro <h.marcelo.del.negro@outlook.com>
2020-11-21 13:14:51 -0300

    tareas

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,17 @@ import { DB } from './DB';
 import { Escritor } from './Escritor';
 import { Lector } from './Lector';
 
+// Fran:
+// 1. Implementar DB, Implementar Escritor, Implementar Lector
+// 2. Armar semaforos
+// 3. Sincronizar todo
+
+// Marce:
+// 1. Implementar TDA cola
+// 2. Implementar FIFO en el planificador
+// 3. Armar el dispatcher
+
+// Despues:
+// 1. Lanzar procesos hardcodeados
+// 2. Agregar dinamicamente procesos
+// 3. Monitorear
